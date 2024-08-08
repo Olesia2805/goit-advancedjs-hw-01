601943c30f601bd0a34188bbac8157d5fb56a37e
Olesia2805 <vysotina2002@gmail.com>
2024-08-08 23:30:21 +0200

    form is done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,39 @@
 const formData = { email: '', message: '' };
+
+const formEl = document.querySelector('.feedback-form');
+
+const isEmpty = form => {
+  const formFromLS = JSON.parse(localStorage.getItem('feedback-form-data'));
+  if (formFromLS !== null) {
+    for (const key in formFromLS) {
+      if (formFromLS.hasOwnProperty(key)) {
+        form.elements[key].value = formFromLS[key];
+        formData[key] = formFromLS[key];
+      }
+    }
+  }
+};
+
+isEmpty(formEl);
+
+formEl.addEventListener('input', event => {
+  const nameField = event.target.name;
+  const valueField = event.target.value.trim();
+
+  formData[nameField] = valueField;
+
+  localStorage.setItem('feedback-form-data', JSON.stringify(formData));
+});
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData['email'] && formData['message']) {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-data');
+    event.target.reset();
+    formData.email = '';
+    formData.message = '';
+  } else {
+    alert('Fill please all fields');
+  }
+});
